83576d2f2129d3e31f27defa487ded5eda67e380
Jose Fegasu <fegasu@misena.edu.co>
2022-09-16 10:04:52 -0500

    Update clonargitrepositorio.cmd

--- VIDEOS/Instalacion/clonargitrepositorio.cmd
+++ VIDEOS/Instalacion/clonargitrepositorio.cmd
@@ -1,1 +1,1 @@
-start chrome https://drive.google.com/file/d/1MhxSyc3lr5XxK2HLP0P6UoGkwyyUDZoV/view?usp=sharing
+start chrome https://drive.google.com/file/d/1liYV40PxKBjgABUc7Y_Phx91vM3_PJt1/view?usp=sharing
